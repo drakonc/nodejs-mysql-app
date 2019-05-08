0be0667cb44f91b1930cf37990705d60d1daf07d
Jose Castro <drakonc@users.noreply.github.com>
2019-05-08 15:16:51 -0500

    Agregando Archivos para las Rutas Y Consultas de base de datos

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 //Configuraciones
 app.set("port", process.env.PORT || 4000);
 app.set("views", path.join(__dirname, "views"));
-app.engine(".hbs", exphbs({
+app.engine(".hbs",exphbs({
     defaultLayout: "main",
     layoutsDir: path.join(app.get("views"), "layouts"),
     partialsDir: path.join(app.get("views"), "partials"),
@@ -26,14 +26,16 @@ app.use(express.json());
 
 //Variables Globales
 app.use((req, res, next) => {
-
-    next();
+  next();
 });
 
 //Rutas
 app.use(require("./routers/index.router"));
+app.use(require("./routers/authenticatio.router"));
+app.use('/links',require("./routers/links.router"));
 
 //Public
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Inicio del Servidor
 app.listen(app.get("port"), () => {
