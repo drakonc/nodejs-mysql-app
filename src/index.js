034441ec620efaecf3b25620ce9e8a739abd4761
Jose Castro <drakonc@users.noreply.github.com>
2019-05-08 12:01:20 -0500

    Configurando La Aplicacion Paso 1

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+//inicializaciones
+const app = express();
+
+//Configuraciones
+app.set("port", process.env.PORT || 4000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(".hbs", exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    helpers: require("./lib/handlebars")
+  })
+);
+app.set("view engine", ".hbs");
+
+//Middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Variables Globales
+app.use((req, res, next) => {
+
+    next();
+});
+
+//Rutas
+app.use(require("./routers/index.router"));
+
+//Public
+
+//Inicio del Servidor
+app.listen(app.get("port"), () => {
+  console.log("Servidor Corriendo en Puerto:", app.get("port"));
+});
