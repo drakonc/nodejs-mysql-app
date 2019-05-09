eb474fd3321469331a232e06a08e19848dd0afa2
Jose Castro <drakonc@users.noreply.github.com>
2019-05-09 14:09:51 -0500

    Funcion de Eliminar

--- src/routers/links.router.js
+++ src/routers/links.router.js
@@ -18,7 +18,15 @@ router.post("/add", async (req, res) => {
   const newLink = { title, url, description };
   console.log(newLink);
   const respuesta = await pool.query("INSERT INTO links SET ?", [newLink]);
-  console.log(respuesta.affectedRows);
+  console.log(respuesta);
+  res.redirect("/links");
+});
+
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const respuesta = await pool.query("DELETE FROM links WHERE id = ?", [id]);
+  console.log(respuesta);
   res.redirect("/links");
 });
 
