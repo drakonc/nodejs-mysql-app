488bf3221f2be5d1cf3e9df429af21e9bc1b5c10
Jose Castro <drakonc@users.noreply.github.com>
2019-05-09 13:44:18 -0500

    Listar y Guardar Links En Base de datos

--- src/routers/links.router.js
+++ src/routers/links.router.js
@@ -3,12 +3,23 @@ const router = express.Router();
 
 const pool = require("../database");
 
+router.get("/", async (req, res) => {
+  const links = await pool.query("SELECT * FROM links");
+  console.log(links);
+  res.render("links/list", { links });
+});
+
 router.get("/add", (req, res) => {
   res.render("links/add");
 });
 
-router.post("/add", (req, res) => {
-  res.send("Formulario Recibido");
+router.post("/add", async (req, res) => {
+  const { title, url, description } = req.body;
+  const newLink = { title, url, description };
+  console.log(newLink);
+  const respuesta = await pool.query("INSERT INTO links SET ?", [newLink]);
+  console.log(respuesta.affectedRows);
+  res.redirect("/links");
 });
 
 module.exports = router;
